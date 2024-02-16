39331a974214252b7391ac03cf3d956bc774beec
Sofia Rodrigues (CTW) <sofia.rodrigues@ctw.bmwgroup.com>
2024-02-16 11:52:43 +0000

    Add karma conf file

---
+++ karma.conf.js
@@ -0,0 +1,14 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+  });
+};
